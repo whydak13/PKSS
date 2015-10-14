66cb4992b84897d4afe46f2fc240163c2405ed3c
omicronns <omicronns@gmail.com>
2015-10-14 22:43:36 +0200

    added json parsing library with an example

--- server/pkss-masters-server/pkss-masters-server/src/main.cpp
+++ server/pkss-masters-server/pkss-masters-server/src/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
 #include <string>
 #include <thread>
-#include <SFML\Network.hpp>
+#include <SFML/Network.hpp>
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
+
+void jsonExample(void) {
+    const char * json = "{\"val1\":\"str\",\"val2\":3}";
+    std::cout << json << std::endl;
+
+    //parse
+    rapidjson::Document doc;
+    doc.Parse(json);
+
+    //modify
+    rapidjson::Value & val1 = doc["val1"];
+    val1.SetDouble(1.345);
+    rapidjson::Value & val2 = doc["val2"];
+    val2.SetInt(val2.GetInt() * 2);
+
+    //serialize
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    doc.Accept(writer);
+
+    std::cout << buffer.GetString() << std::endl;
+}
 
 int main(int argc, char ** argv) {
+    jsonExample();
     sf::TcpListener oListener;
     uint8_t aBuffer[1024];
     size_t iSize;
